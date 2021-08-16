8763afadcb85c9bbeef07fce90f5f989c204abb6
KostiantynNaumenko <naumenko.kostiantyn@gmail.com>
2021-08-16 12:20:14 +0300

    adding index page and controller

---
+++ src/test/java/springframework/recipeproject/RecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package springframework.recipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
