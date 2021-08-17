be78b6cf64b52ada2ee111f2c972d3fb71ed1c9c
KostiantynNaumenko <naumenko.kostiantyn@gmail.com>
2021-08-17 14:23:24 +0300

    managed 1-* relationship. closes #4

--- src/main/java/springframework/recipeproject/domain/Ingredient.java
+++ src/main/java/springframework/recipeproject/domain/Ingredient.java
@@ -1,7 +1,6 @@
 package springframework.recipeproject.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
@@ -9,11 +8,15 @@ import java.math.BigInteger;
 public class Ingredient {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String description;
     private BigDecimal amount;
 
+    //private UnitOfMeasure uom;
+
+    @ManyToOne()
     private Recipe recipe;
 
     public Long getId() {
