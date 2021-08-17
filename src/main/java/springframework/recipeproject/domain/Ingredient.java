0a8392c733a19f13634ea5323ec3d074efd42f31
KostiantynNaumenko <naumenko.kostiantyn@gmail.com>
2021-08-17 14:17:46 +0300

    adding ingredient entity. closes #3

---
+++ src/main/java/springframework/recipeproject/domain/Ingredient.java
@@ -0,0 +1,50 @@
+package springframework.recipeproject.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    private Long id;
+
+    private String description;
+    private BigDecimal amount;
+
+    private Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
